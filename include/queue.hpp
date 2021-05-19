253693d5d42e1b61bb711e155b2d8f21f8905953
StarBeats <977663818@qq.com>
2021-05-19 20:21:15 +0800

    [o]统一代码风格
    using cpplint

--- include/queue.hpp
+++ include/queue.hpp
@@ -1,5 +1,33 @@
+/*
+#Copyright 2021 xyang.
+*/
+
 #pragma once
+#include <queue>
+#include <shared_mutex>
+
 namespace X::XAlgorithm
 {
-    
-} 
+template<typename T>
+struct Queue
+{
+ private:
+    std::queue<T> _queue;
+};
+template<typename T>
+struct RWSpan
+{
+    void set(T v)
+    {
+        std::unique_lock lock(_stmutex);
+    }
+    T& get()
+    {
+        std::shared_lock lock(_stmutex);
+        return vaule;
+    }
+ private:
+    std::shared_timed_mutex _stmutex;
+    T vaule;
+};
+}  // namespace X::XAlgorithm
